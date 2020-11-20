74813d9395cb76f928c4d3ae42f8fc7c4279bd0b
rax <rax4927sd@gmail.com>
2020-11-20 11:09:58 +0200

    Добавьте файлы проекта.

---
+++ ClassGame.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ugadayka
+{
+    class ClassGame
+    {
+
+        // Текущее значение жизни
+        private int counter;
+
+        public int Counter
+        {
+            get
+            {
+                return counter;
+            }
+            private set
+            {
+                counter = value;
+            }
+        }
+
+
+        // Состояние запуска игры
+        private bool isGameStart = false;
+
+        public bool IsGameStart
+        {
+            get
+            {
+                return isGameStart;
+            }
+            private set
+            {
+                isGameStart = value;
+            }
+        }
+
+        // Если победа
+        private bool isWin = false;
+
+        // Если проигрыш
+        private bool isLose = false;
+
+        // Кол-во жизней
+        private int lives;
+
+        public int Lives
+        {
+            get
+            {
+                return lives;
+            }
+            set
+            {
+                lives = value;
+            }
+        }
+
+        // Задуманное число
+        private int desired;
+
+        public int Desired
+        {
+            get
+            {
+                return desired;
+            }
+            private set
+            {
+                desired = value;
+            }
+        }
+
+        public ClassGame(int lives)
+        {
+            Lives = lives;
+        }
+
+        private void LiveDown()
+        {
+            counter--;
+            if (counter < 0)
+                counter = 0;
+        }
+
+        private void GenerateNumber()
+        {
+            Random rnd = new Random();
+            Desired = rnd.Next(1, 10);
+        }
+
+        public void StartGame()
+        {
+            counter = Lives;
+            GenerateNumber();
+            isGameStart = true;
+            isWin = false;
+            isLose = false;
+        }
+
+        public void NextStep(int val)
+        {
+            if(val != Desired)
+            {
+                LiveDown();
+
+                if(counter == 0)
+                {
+                    isGameStart = false;
+                    isLose = true;
+                }
+            }
+            else
+            {
+                isGameStart = false;
+                isWin = true;
+            }
+        }
+
+
+        public int checkGameStatus()
+        {
+            int code = 0;
+
+            if(isWin)
+            {
+                code = 1;
+            }
+            else if(isLose)
+            {
+                code = 2;
+            }
+            else if(isGameStart)
+            {
+                code = 3;
+            }
+
+            return code;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ugadayka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ClassGame classGame;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            classGame = new ClassGame(5);
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            classGame.StartGame();
+
+            counterText.Inlines.Clear();
+            counterText.Inlines.Add(new Run("Кол-во попыток:") {
+                FontWeight = FontWeights.Bold,
+                FontSize = 20
+            });
+            counterText.Inlines.Add(new Run(classGame.Counter.ToString())
+            {
+                FontWeight = FontWeights.Black,
+                FontSize = 44,
+                Foreground = Brushes.Red
+            });
+        }
+
+        private void ButtonRestartClick(object sender, RoutedEventArgs e)
+        {
+            StartGame();
+        }
+
+        private void ButtonNumberClick(object sender, RoutedEventArgs e)
+        {
+            int codeStatus = classGame.checkGameStatus();
+            MessageBoxResult msgResult;
+
+            if (codeStatus != 3)
+            {
+                msgResult = MessageBox.Show("Запустить игру?", "Запустить игру?", MessageBoxButton.OKCancel);
+                if(msgResult == MessageBoxResult.OK)
+                {
+                    StartGame();
+                }
+            }
+            else
+            {
+                string curVal = (string)((Button)e.OriginalSource).Content;
+                classGame.NextStep(Int32.Parse(curVal));
+
+                counterText.Inlines.Clear();
+                counterText.Inlines.Add(new Run("Кол-во попыток:")
+                {
+                    FontWeight = FontWeights.Bold,
+                    FontSize = 20
+                });
+                counterText.Inlines.Add(new Run(classGame.Counter.ToString())
+                {
+                    FontWeight = FontWeights.Black,
+                    FontSize = 44,
+                    Foreground = Brushes.Red
+                });
+
+                codeStatus = classGame.checkGameStatus();
+
+                if(codeStatus == 1)
+                {
+                    msgResult = MessageBox.Show("ПОБЕДА :) \n Запустить игру?", "ПОБЕДА :)", MessageBoxButton.OKCancel);
+                    if (msgResult == MessageBoxResult.OK)
+                    {
+                        StartGame();
+                    }
+                }
+                else if(codeStatus == 2)
+                {
+                    msgResult = MessageBox.Show("ПОРАЖЕНИЕ :( \n Запустить игру?", "ПОРАЖЕНИЕ :(", MessageBoxButton.OKCancel);
+                    if (msgResult == MessageBoxResult.OK)
+                    {
+                        StartGame();
+                    }
+                }
+
+            }
+        }
+
+    }
+}
